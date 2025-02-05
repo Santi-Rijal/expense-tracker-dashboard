d48a7d7fa5fff0a6271f8efe08d9bedf06c40217
Santi <rijalsantiram@gmail.com>
2025-02-05 13:03:00 -0500

    added top 5 expense chart

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,17 @@ import Wallet from "@/components/Wallet/Wallet";
 import styles from "./page.module.css";
 import UserCard from "@/components/UserCard/UserCard";
 import IncomeToExpenseRatio from "@/components/IncomeToExpenseRatio/IncomeToExpenseRatio";
+import TopFiveExpensesChart from "@/components/TopFiveExpensesChart/TopFiveExpensesChart";
 
 export default function Home() {
   return (
     <main className={styles.mainContainer}>
-      <UserCard />
-      <div className={styles.walletChartContainer}>
+      <div className={styles.leftSection}>
+        <UserCard />
+        <TopFiveExpensesChart />
+      </div>
+
+      <div className={styles.rightSection}>
         <Wallet />
         <IncomeToExpenseRatio />
       </div>
---
+++ src/components/TopFiveExpensesChart/TopFiveExpensesChart.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import styles from "./TopFiveExpensesChart.module.css";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const data = {
+  labels: ["Rent", "Groceries", "Transport", "Entertainment", "Utilities"],
+  datasets: [
+    {
+      label: "Expense ($)",
+      data: [800, 300, 100, 150, 120],
+      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4CAF50", "#9C27B0"],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const TopFiveExpensesChart = () => {
+  return (
+    <div className={styles.topFiveContainer}>
+      <h3>Top 5 Expense Sources</h3>
+      <Bar data={data} options={{ borderColor: "#fff" }} />
+    </div>
+  );
+};
+
+export default TopFiveExpensesChart;
