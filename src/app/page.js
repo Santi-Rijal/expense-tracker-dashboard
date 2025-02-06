241a1b0487eb6dd91de069413b08e9d6257efe4d
Santi <rijalsantiram@gmail.com>
2025-02-06 03:51:25 -0500

    added input card

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+"use client";
+
 import Wallet from "@/components/Wallet/Wallet";
 import styles from "./page.module.css";
 import UserCard from "@/components/UserCard/UserCard";
 import IncomeToExpenseRatio from "@/components/IncomeToExpenseRatio/IncomeToExpenseRatio";
 import TopFiveExpensesChart from "@/components/TopFiveExpensesChart/TopFiveExpensesChart";
 import MonthlyExpense from "@/components/MonthlyExpense/MonthlyExpense";
+
+import { IoMdSend } from "react-icons/io";
 
 export default function Home() {
   return (
@@ -22,6 +26,24 @@ export default function Home() {
       <div className={styles.rightSection}>
         <Wallet />
         <IncomeToExpenseRatio />
+        <div className={styles.userInput}>
+          <div className={styles.nav}>
+            <p>Income</p>
+            <p>Expense</p>
+          </div>
+
+          <div className={styles.inputContainer}>
+            <input
+              type="number"
+              placeholder="Value"
+              min="0"
+              onInput={(e) => (e.target.value = Math.abs(e.target.value))}
+            />
+            <button>
+              <IoMdSend />
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   );
